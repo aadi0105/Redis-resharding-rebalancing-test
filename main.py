02e197f5b4c4551d5c6d4f011917b1aa32a9f2d5
Prateek Sanodiya <prateeksanodiya937@gmail.com>
2024-05-09 21:52:36 +0530

    added time in every step

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ def wait(seconds):
     print(f"Waiting for {seconds} seconds...")
     sleep(seconds)
 
+def print_time(time_taken):
+    print("Time Taken:", time_taken)
+
 def main(nodes):
     #console log time stamp and numner of keys tested with
     t1 = time()
@@ -25,52 +28,64 @@ def main(nodes):
     print("1 > EMPTY CLUSTER OF THREE NODES\n\n")
     result = create_cluster.create_cluster(nodes)
     print(f"{result}\n")
+    print_time(time()-t1)
 
     wait(10)
     count(2)
 
+    st1 = time()
     #we need to wait for some time before writing keys otherwise cluster down error occurs
     print(f"3 > WRITE KEYS\n\n")
     result = write_keys.write_keys(nodes)
     print(f"{result}\n")
+    print_time(time() - st1)
     
     count(4)
     
+    st1 = time()
     print("5 > ADD MASTER\n\n")
     result = add_master.add_master(nodes["node1"]["ip"],6379,nodes["node4"]["ip"],6379)
     print(f"{result}\n" )
+    print_time(time() - st1)
 
     wait(10) #this wait is necessary. it doesn't show up in count otherwise.
     count(6)
 
+    st1 = time()
     print("7 > REBALANCE\n\n")
     result= rebalance.rebalance_cluster(nodes["node4"]["ip"], nodes["node4"]["port"])
     print(f"{result}\n" )
+    print_time(time() - st1)
     
     count(8)
 
     wait(15)
+    st1 = time()
     print("9> RESHARD\n\n")
     result= reshard.reshard(nodes["node1"]["ip"], nodes["node1"]["port"], nodes["node4"]["ip"], nodes["node4"]["port"])
     print(f"{result}\n" )
+    print_time(time() - st1)
     
-    count(10)
-
     wait(15)
+    count(10)
+    
+    st1 = time()
     print("11 > REBALANCE\n\n")
     result= rebalance.rebalance_cluster(nodes["node4"]["ip"], nodes["node4"]["port"])
     print(f"{result}\n" )
+    print_time(time() - st1)
 
     count(12)
 
+    st1 = time()
     print("13 > REMOVE 4TH NDOE\n\n")
     result= remove_master.remove_master(nodes["node4"]["ip"], nodes["node4"]["port"])
     print(f"{result}\n" )
+    print_time(time() - st1)
 
     count(14)
-    t2 = time()
-    print(t2)
-    print("Time Taken:", t2-t1)
+    
+    print_time(time() - t1)
 
 
 file = open("./inventory.json")
